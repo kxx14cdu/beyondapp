12e632aca16164803997268079b56e103fe0cfc1
unknown-user <unknown-user@192.168.20.29>
2015-07-28 09:38:14 +0100

    Initial Setup

---
+++ bfb.net/app.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace bfb.net
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			
+		}
+
+		protected override void OnStart ()
+		{
+			
+		}
+
+		protected override void OnSleep ()
+		{
+			
+		}
+
+		protected override void OnResume ()
+		{
+			
+		}
+	}
+}
+
